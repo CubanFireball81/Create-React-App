d3b9807e54f247085dfb23b2a9eadbbb70b9629e
Alejandro Santiago <cubanfireball81@gmail.com>
2021-12-21 23:12:35 -0900

    React Router

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({ campsite, onClick }) {
+function RenderDirectoryItem({ campsite}) {
     return (
-        <Card onClick={() => onClick(campsite.id)}>
+        <Card>
             <CardImg width='100%' src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
                 <CardTitle>{campsite.name}</CardTitle>
             </CardImgOverlay>
         </Card>
-    )
+    );
 }
 
 function Directory(props) {
     const directory = props.campsites.map(campsite => {
         return (
             <div key={campsite.id} className='col-md-5 m-1'>
-                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+                <RenderDirectoryItem campsite={campsite} />
             </div>
-        )
+        );
     })
 
     return (
         <div className='container'>
             <div className='row'>{directory}</div>
         </div>
-    )
+    );
 }
 
-export default Directory
+export default Directory;
