44d104fe145651a6cfd3600e517401aec96b7c1b
Alejandro Santiago <cubanfireball81@gmail.com>
2021-12-21 22:29:41 -0900

    Header and Footer

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Container } from 'reactstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Container fluid>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col'>
+                                <h1>NuCamp</h1>
+                                <h2>a better way to camp</h2>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+
+                <Navbar dark sticky='top'>
+                    <div className='container'>
+                        <NavbarBrand href='/'>NuCamp</NavbarBrand>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
