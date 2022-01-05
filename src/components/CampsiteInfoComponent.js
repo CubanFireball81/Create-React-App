8ef99cc46eb924b71fbd1f816ce6f3f27021f9da
Alejandro Santiago <cubanfireball81@gmail.com>
2022-01-05 14:25:20 -0900

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,11 +21,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        //Use JSON.stringify to turn JavaScript object data into a string
-        //console.log('Current state is: ' + JSON.stringify(values));
-        //alert('Current state is: ' + JSON.stringify(values));
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     toggleModal() {
@@ -109,7 +106,7 @@ function RenderCampsite({ campsite }) {
     )
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <div className='col-md-5 m-1'>
@@ -131,7 +128,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                     )
                 })}
                 <div>
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
             </div>
         )
@@ -177,9 +174,9 @@ function CampsiteInfo(props) {
                 </div>
                 <div className='row'>
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
+                    <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,13 +6,14 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { addComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
 
 
 const mapDispatchToProps = {
+    postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
     addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
     fetchCampsites: () => (fetchCampsites()),
     resetFeedbackForm: () => (actions.reset('feedbackForm')),
@@ -61,7 +62,7 @@ class Main extends Component {
                     errMess={this.props.campsites.errMess}
                     comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                     commentsErrMess={this.props.comments.errMess}
-                    addComment={this.props.addComment}
+                    postComment={this.props.postComment}
                 />
             );
         };
